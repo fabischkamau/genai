c87eae187f38458586209f9ee9d68eaaa45b2cac
kamaufabisch1175 <kamaufabisch1175@gmail.com>
2024-07-17 14:18:34 +0000

    root change

--- app/root.tsx
+++ app/root.tsx
@@ -4,18 +4,14 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-  useLoaderData,
+
 } from "@remix-run/react";
 import "./tailwind.css";
 import { Toaster } from "./components/ui/toaster";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-export async function loader() {
-  return {
-    clientId: process.env.CLIENT_ID,
-  };
-}
+
 export function Layout({ children }: { children: React.ReactNode }) {
-  const loaderData = useLoaderData<typeof loader>();
+
 
   return (
     <GoogleOAuthProvider clientId="900174728297-b3asta45ta4h7vbvlpd9dqdkjvubuule.apps.googleusercontent.com">
